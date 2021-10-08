fad3476852de7d8ccff7962a5f10bb80ec2f1b0b
Karni Shekhawat <Karnisinghshekhawat@protonmail.com>
2021-10-08 11:08:21 +0200

    Working fine
    Starting the cleaning

---
+++ Data/CustomIDataProtection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Portal_Azure_AD.Data
+{
+    public class CustomIDataProtection
+    {
+        private readonly IDataProtector protector;
+
+
+        public CustomIDataProtection(IDataProtectionProvider dataProtectionProvider, UniqueCode uniqueCode)
+        {
+            protector = dataProtectionProvider.CreateProtector(uniqueCode.UrlKey);
+        }
+        public string Decode(string data)
+        {
+            return protector.Protect(data);
+        }
+        public string Encode(string data)
+        {
+            Console.WriteLine(data + "<--Data ");
+            return protector.Unprotect(data);
+        }
+        
+    }
+
+    
+}
---
+++ Data/UniqueCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDF_Portal_Azure_AD.Data
+{
+    public class UniqueCode
+    {
+        public readonly string UrlKey = "ChiaveEPF_Pubblica";
+
+    }
+   
+}
